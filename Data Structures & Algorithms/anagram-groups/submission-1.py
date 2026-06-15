ab08e765b3074f7e2266ebffff61fa999d7fe1d3
VINOD KUMAR N <an2743055@gmail.com>
2026-06-15 10:01:58 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+       from collections import defaultdict
+       anagram_map=defaultdict(list)
+       for words in strs:
+        sorted_keys="".join(sorted(words))
+        anagram_map[sorted_keys].append(words) 
+       return list(anagram_map.values())
+               
+            
